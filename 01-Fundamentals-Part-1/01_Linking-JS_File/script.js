59ee9907cf066826160160d8be9af9e42b1d8e01
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-11-06 21:00:41 +0530

    01_Linking-JS_File

---
+++ 01-Fundamentals-Part-1/01_Linking-JS_File/script.js
@@ -0,0 +1,5 @@
+let js = "amazing";
+if (js === "amazing") alert("JS is amazing");
+
+40 + 2 + 10 - 25; // This will not show the accepted result in the console. To see the result, we need to use console.log()
+console.log(40 + 2 + 10 - 25);
