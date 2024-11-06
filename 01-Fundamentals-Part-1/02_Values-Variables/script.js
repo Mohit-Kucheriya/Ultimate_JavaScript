6589e8d603758b7173e3207642b369340bec4b63
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-11-06 21:01:04 +0530

    02_Values-Variables

---
+++ 01-Fundamentals-Part-1/02_Values-Variables/script.js
@@ -0,0 +1,34 @@
+let js = "amazing";
+// Here js is a variable & it stores the value "amazing"
+
+console.log("JavaScript is FUN!"); // JavaScript is FUN! is also a value
+
+console.log(23);
+
+// By storing the value in a variable, we can use it later and use it for multiple times.
+let firstName = "Mohit";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variables names as descriptive as possible.
+let myFirstJob = "Software Tester";
+let myCurrentJob = "JavaScript Developer";
+
+
+// Assignment 
+/*
+1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+2. Log their values to the console.
+*/
+
+let country = "India";
+let continent = "Asia";
+let population = 1300;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+
