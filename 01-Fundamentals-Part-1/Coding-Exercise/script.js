eebb28a8aadad9876f09861a6307ac5d38753c2d
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-11-07 15:50:52 +0530

    Coding-Exercise

--- 01-Fundamentals-Part-1/Coding-Exercise/script.js
+++ 01-Fundamentals-Part-1/Coding-Exercise/script.js
@@ -1,5 +1,6 @@
 /*
 
+Challenge 1
 Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
 
 Your task is to write some code to help them:
