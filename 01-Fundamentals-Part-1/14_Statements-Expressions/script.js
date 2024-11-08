db51766d769eb7a7b8d9ac291c6368081837a527
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-11-08 19:19:03 +0530

    14_Statements-Expressions

---
+++ 01-Fundamentals-Part-1/14_Statements-Expressions/script.js
@@ -0,0 +1,19 @@
+/*
+
+Expressions - An expression is a piece of code that evaluates to a value.
+
+Statement  - A statement is a piece of code that performs an action.
+
+*/
+
+3 + 4; // Expression
+
+let y = 5; //variable declaration
+
+// conditional Statement
+if (23 > 10) {
+    const bigger = `23 is bigger than 10`;
+}
+
+console.log(`I'm ${2024 - 2001} year old!`); // This is an expression
+
