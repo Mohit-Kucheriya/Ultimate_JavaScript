b9b3d236d1dec12f5a0e50a3b5625185b35dd9bc
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-12-03 13:28:44 +0530

    CHALLENGE #2

--- 05-Data-Structures_Modern-Operators_Strings/Coding-Exercise/script.js
+++ 05-Data-Structures_Modern-Operators_Strings/Coding-Exercise/script.js
@@ -79,7 +79,6 @@ const game = {
         x: 3.25,
         team2: 6.5,
     },
-
 };
 
 // Step 1: Create one player array for each team
@@ -101,7 +100,7 @@ const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
 console.log(players1Final);
 
 // Step 5: Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
-const { team1, x: draw, team2 } = game.odds
+const { team1, x: draw, team2 } = game.odds;
 console.log(team1);
 console.log(draw);
 console.log(team2);
@@ -110,13 +109,97 @@ console.log(team2);
 const printGoals = function (...players) {
     console.log(players);
     console.log(`Total Goals: ${players.length}`);
-}
-printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich')
-printGoals(...game.scored)
+};
+printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+printGoals(...game.scored);
 
 // Step 7: The team with the lower odd is more likely to win. Print to the console which team is more likely to win, without using an if/else statement or the ternary operator.
-team1 < team2 && console.log(`Team 1 is more likely to win`)
-team1 > team2 && console.log(`Team 2 is more likely to win`)
+team1 < team2 && console.log(`Team 1 is more likely to win`);
+team1 > team2 && console.log(`Team 2 is more likely to win`);
 
+/*
 
+CHALLENGE #2
 
+Let's continue with our football betting app! Keep using the 'game' variable from
+before.
+
+Your tasks -
+1. Loop over the game.scored array and print each player name to the console,
+along with the goal number (Example: "Goal 1: Lewandowski")
+
+2. Use a loop to calculate the average odd and log it to the console (We already
+studied how to calculate averages, you can go check if you don't remember)
+
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+
+Odd of victory Bayern Munich: 1.33
+Odd of draw: 3.25
+Odd of victory Borrussia Dortmund: 6.5
+
+Get the team names directly from the game object, don't hardcode them
+(except for "draw"). Hint: Note how the odds and the game objects have the
+same property names �
+
+4. Bonus: Create an object called 'scorers' which contains the names of the
+players who scored as properties, and the number of goals as the value. In this
+game, it will look like this:
+{
+ Gnarby: 1,
+ Hummels: 1,
+ Lewandowski: 2
+}
+
+*/
+
+// Task 1: Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+
+// Method 1 - Using for loop and indexOf method
+for (const player of game.scored) {
+    console.log(`Goal ${game.scored.indexOf(player) + 1}: ${player}`);
+}
+
+// Method 2 - Using for-of loop and entries method
+const playerGoals = [...game.scored];
+console.log(playerGoals);
+
+for (const [index, eachPlayer] of playerGoals.entries()) {
+    console.log(`Goal ${index + 1}: ${eachPlayer}`);
+}
+
+// Task 2: Use a loop to calculate the average odd and log it to the console
+let average = 0;
+const odds = Object.values(game.odds);
+console.log(odds);
+
+for (const odd of odds) {
+    average += odd;
+}
+console.log(average / odds.length);
+
+// Task 3: Print the 3 odds
+for (const [team, odd] of Object.entries(game.odds)) {
+    console.log(team, odd);
+    const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
+    console.log(`Odd of ${teamStr}: ${odd}`);
+}
+
+// Task 4: BONUS
+/*
+
+When a player's name doesn't exist in scorers -
+The ternary operator checks scorers[player]. If it is undefined (doesn't exist), 
+the second part of the ternary (scorers[player] = 1) runs.
+
+This creates the property with the key as the player's name and initializes its value to 1.
+
+Properties are created on-the-fly when assigned.
+
+*/
+const scorers = {};
+
+for (const player of game.scored) {
+    console.log(player);
+    scorers[player] ? scorers[player]++ : (scorers[player] = 1)
+}
+console.log(scorers);
