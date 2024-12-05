d9d222d75b750e6b0cc478fc7f6e1442b96686af
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-12-05 12:55:07 +0530

    CHALLENGE #3

--- 05-Data-Structures_Modern-Operators_Strings/Coding-Exercise/script.js
+++ 05-Data-Structures_Modern-Operators_Strings/Coding-Exercise/script.js
@@ -3,39 +3,39 @@
 CHALLENGE #1 - Football Betting App
 
 We're building a football betting app (soccer for my American friends �)!
-Suppose we get data from a web service about a certain game ('game' variable on 
+Suppose we get data from a web service about a certain game ('game' variable on
 next page). In this challenge we're gonna work with that data.
 
 Your tasks:
 
-1. Create one player array for each team (variables 'players1' and 
+1. Create one player array for each team (variables 'players1' and
 'players2')
 
-2. The first player in any player array is the goalkeeper and the others are field 
-players. For Bayern Munich (team 1) create one variable ('gk') with the 
-goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 
+2. The first player in any player array is the goalkeeper and the others are field
+players. For Bayern Munich (team 1) create one variable ('gk') with the
+goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
 field players
 
-3. Create an array 'allPlayers' containing all players of both teams (22 
+3. Create an array 'allPlayers' containing all players of both teams (22
 players)
 
-4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a 
-new array ('players1Final') containing all the original team1 players plus 
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+new array ('players1Final') containing all the original team1 players plus
 'Thiago', 'Coutinho' and 'Perisic'
 
-5. Based on the game.odds object, create one variable for each odd (called 
+5. Based on the game.odds object, create one variable for each odd (called
 'team1', 'draw' and 'team2')
 
-6. Write a function ('printGoals') that receives an arbitrary number of player 
-names (not an array) and prints each of them to the console, along with the 
+6. Write a function ('printGoals') that receives an arbitrary number of player
+names (not an array) and prints each of them to the console, along with the
 number of goals that were scored in total (number of player names passed in)
 
-7. The team with the lower odd is more likely to win. Print to the console which 
-team is more likely to win, without using an if/else statement or the ternary 
+7. The team with the lower odd is more likely to win. Print to the console which
+team is more likely to win, without using an if/else statement or the ternary
 operator.
 
 Test data for 6.
- First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. 
+ First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
 Then, call the function again with players from game.scored
 
 */
@@ -188,7 +188,7 @@ for (const [team, odd] of Object.entries(game.odds)) {
 /*
 
 When a player's name doesn't exist in scorers -
-The ternary operator checks scorers[player]. If it is undefined (doesn't exist), 
+The ternary operator checks scorers[player]. If it is undefined (doesn't exist),
 the second part of the ternary (scorers[player] = 1) runs.
 
 This creates the property with the key as the player's name and initializes its value to 1.
@@ -200,6 +200,67 @@ const scorers = {};
 
 for (const player of game.scored) {
     console.log(player);
-    scorers[player] ? scorers[player]++ : (scorers[player] = 1)
+    scorers[player] ? scorers[player]++ : (scorers[player] = 1);
 }
 console.log(scorers);
+
+/*
+
+CHALLENGE #3
+Let's continue with our football betting app! This time, we have a map called 
+'gameEvents' (see below) with a log of the events that happened during the 
+game. The values are the events themselves, and the keys are the minutes in which 
+each event happened (a football game has 90 minutes plus some extra time).
+
+Your tasks -
+
+1. Create an array 'events' of the different game events that happened (no 
+duplicates)
+
+2. After the game has finished, is was found that the yellow card from minute 64 
+was unfair. So remove this event from the game events log.
+
+3. Compute and log the following string to the console: "An event happened, on 
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+
+4. Loop over 'gameEvents' and log each element to the console, marking 
+whether it's in the first half or second half (after 45 min) of the game, like this:
+[FIRST HALF] 17: ⚽ GOAL
+
+*/
+
+const gameEvents = new Map([
+    [17, "⚽ GOAL"],
+    [36, "� Substitution"],
+    [47, "⚽ GOAL"],
+    [61, "� Substitution"],
+    [64, "� Yellow card"],
+    [69, "� Red card"],
+    [70, "� Substitution"],
+    [72, "� Substitution"],
+    [76, "⚽ GOAL"],
+    [80, "⚽ GOAL"],
+    [92, "� Yellow card"],
+]);
+
+// Task 1: Create an array 'events' of the different game events that happened (no duplicates)
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// Task 2: After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+console.log(gameEvents.delete(64));
+
+// Task 3: Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+
+console.log(
+    `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+// Task 4: Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+// [FIRST HALF] 17: ⚽ GOAL
+for (const [min, event] of gameEvents) {
+    const half = min <= 45 ? "FIRST HALF" : "SECOND HALF";
+    console.log(`[${half}] ${min}: ${event}`);
+}
