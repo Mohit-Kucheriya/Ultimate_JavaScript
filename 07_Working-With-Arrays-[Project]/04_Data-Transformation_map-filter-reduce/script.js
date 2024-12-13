160fdc725d4d1394008adf5cc4a21a34e2e03219
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-12-13 22:30:19 +0530

    04_Data-Transformation_map-filter-reduce

---
+++ 07_Working-With-Arrays-[Project]/04_Data-Transformation_map-filter-reduce/script.js
@@ -0,0 +1,16 @@
+/*
+
+1. map() -
+map returns a "new array" containing the results of applying an operation on all original
+array elements.
+
+2. filter() -
+filter returns a "new array" containing the array elements that passed a specified test condition.
+OR
+elements for which the condition is true will be included in anew array.
+
+3. reduce() - 
+reduce boils (reduces) all array elements down to one single value (e.g. adding all elements together)
+
+
+*/
